ab96d46b1b61bd9ecb9c15c2e56f4ed4e627a4fe
mehrsam <mehrsammirshekar@gmail.com>
2026-05-13 16:03:16 +0430

    work with database
    added things to database

--- blog/models.py
+++ blog/models.py
@@ -4,15 +4,35 @@ from django.db import models
 class Post(models.Model): #table name 
     title   = models.CharField (max_length=255)
     content =  models.TextField()#column
-    image = models.CharField()
-    category = models.CharField()
-    tag = models.CharField()
-    author = models.CharField()
-    counted_view = models.IntegerField()
-    status = models.BooleanField()
-    published_date = models.DateTimeField()
-    created_date = models.DateTimeField
-    updated_date = models.DateTimeField()
+    # image = models.CharField()
+    # category = models.IntegerField()
+    # tag = models.IntegerField()
+    # author = models.IntegerField()
+    counted_view = models.IntegerField(default=0)#default=0
+    status = models.BooleanField(default=False)
+    published_date = models.DateTimeField(null=True)
+    created_date = models.DateTimeField(auto_now_add=True )
+    updated_date = models.DateTimeField(auto_now=True)
+# class User(models.Model):
+#     username = models.CharField()
+#     firstname = models.CharField()
+#     lastname = models.CharField()
+#     email = models.CharField()
+#     active = models.BooleanField()
+#     superuser = models.BooleanField()
+#     staff = models.BooleanField()
 
-# class  Mehrta(models.Model):
+# class Category(models.Model):
+#     name = models.CharField()
+# class Tag(models.Model):
+#     name = models.CharField()
+
+# class Contact(models.Model):
+#     name = models.CharField()
+#     subject = models.CharField()
+#     email = models.CharField()
+#     massage = models.TextField()
+#     created_date = models.DateTimeField()
+#     updated_date  = models.DateTimeField()
+# # class  Mehrta(models.Model):
 #     name = models.CharField(max_length=250) 
