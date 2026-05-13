2f2b57881dc70a67ad909dc81cf3e80e0400ca28
mehrsam <mehrsammirshekar@gmail.com>
2026-05-13 15:09:39 +0430

    created database
    created database for   blog

--- blog/models.py
+++ blog/models.py
@@ -1,3 +1,18 @@
 from django.db import models
 
 # Create your models here.
+class Post(models.Model): #table name 
+    title   = models.CharField (max_length=255)
+    content =  models.TextField()#column
+    image = models.CharField()
+    category = models.CharField()
+    tag = models.CharField()
+    author = models.CharField()
+    counted_view = models.IntegerField()
+    status = models.BooleanField()
+    published_date = models.DateTimeField()
+    created_date = models.DateTimeField
+    updated_date = models.DateTimeField()
+
+# class  Mehrta(models.Model):
+#     name = models.CharField(max_length=250) 
