0500ebb852abb5fb45eb83155ff4bc4a915d82ec
mehrsam <mehrsammirshekar@gmail.com>
2026-05-19 11:16:58 +0430

    create form
    created form  in django and csrf_token

--- website/views.py
+++ website/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render
 from django.http import HttpResponse , JsonResponse
+from website.models import Contact 
 
 def  index_view(request):
     return render(request , 'website/index.html')
@@ -11,6 +12,23 @@ def contact_view(request):
     return render(request,'website/contact.html')
 
 def test_view(request):
+    if request.method == 'POST':
+        name = request.POST.get('name')
+        email = request.POST.get('email')
+        subject = request.POST.get('subject')
+        massage = request.POST.get('massage')
+        c = Contact()
+        c.name = name
+        c.email = email
+        c.subject = subject
+        c.massage = massage 
+        c.save()
+        print(name , email , subject , massage)
+
+  
+
+
     return render(request,'website/test.html')
 
 # Create your views here.
+  
