cacb545550b8269e47941b310d18d328d48f4f8c
mehrsam <mehrsammirshekar@gmail.com>
2026-05-20 16:24:24 +0430

    work with django module
    work with humanize and  django_extensions

--- mysite/settings.py
+++ mysite/settings.py
@@ -37,6 +37,8 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'django.contrib.humanize', 
+    'django_extensions' ,
     'website.apps.WebsiteConfig' ,
     'blog'
 ]
