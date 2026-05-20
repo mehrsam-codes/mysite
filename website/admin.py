eb4c450a51168528cf18490dd92fe28598128283
mehrsam <mehrsammirshekar@gmail.com>
2026-05-20 15:36:43 +0430

    work with forms
    just create a form for contact and  create a massage in form

--- website/admin.py
+++ website/admin.py
@@ -1,5 +1,5 @@
 from django.contrib import admin
-from website.models import Contact
+from website.models import Contact  , Newsletter
 # Register your models here.
 class ContactAdmin(admin.ModelAdmin):
     date_hierarchy = 'created_date'
@@ -7,4 +7,5 @@ class ContactAdmin(admin.ModelAdmin):
     list_filter = ('email' , )
     search_fields = ('name' , 'massage')
     
-admin.site.register(Contact , ContactAdmin)
+admin.site.register(Contact , ContactAdmin )
+admin.site.register(Newsletter)
--- website/views.py
+++ website/views.py
@@ -1,7 +1,8 @@
 from django.shortcuts import render
-from django.http import HttpResponse , JsonResponse
+from django.http import HttpResponse , JsonResponse , HttpResponseRedirect
 from website.models import Contact 
-from website.forms import NameForm , ContactForm
+from website.forms import NameForm , ContactForm , NewsletterForm
+from django.contrib import messages
 def  index_view(request):
     return render(request , 'website/index.html')
 
@@ -9,7 +10,15 @@ def about_view(request):
     return render(request , 'website/about.html')
 
 def contact_view(request):
-    return render(request,'website/contact.html')
+    if request.method == 'POST':
+        form = ContactForm(request.POST)
+        if form.is_valid():
+            form.save()
+            messages.add_message(request  , messages.SUCCESS , 'your ticket submited successfully')
+        else:
+            messages.add_message(request  , messages.ERROR , 'your ticket didnt submited successfully')
+    form = ContactForm()
+    return render(request,'website/contact.html', {'form':form})
 
 def test_view(request):
     if request.method == 'POST':
@@ -21,6 +30,13 @@ def test_view(request):
             return HttpResponse("not valid")
     form = ContactForm()
     return render(request,'website/test.html' , {'form':form})
-
+def newsletter_view(request):
+    if request.method == 'POST':
+        form = NewsletterForm(request.POST)
+        if form.is_valid():
+            form.save()
+            return  HttpResponseRedirect('/')
+    else:
+        return HttpResponseRedirect('/')
 # Create your views here.
   
